1a43cf17758cd9aee4b967512010efeb37510822
hoangnv2710 <vanhoangk27@gmail.com>
2024-11-25 21:11:04 +0700

    dl

--- src/Process.java
+++ src/Process.java
@@ -32,7 +32,6 @@ public class Process {
                     int g = (rgb >> 8) & 0xFF;
                     int b = rgb & 0xFF;
                     int gray = (int) (0.299 * r + 0.587 * g + 0.114 * b);
-//                    int grayRgb = (gray << 16) | (gray << 8) | gray; grayImage.setRGB(x, y, grayRgb);
                     grayMatrix[x][y] = gray;
                     raster.setSample(x, y, 0, gray);
                 }
@@ -51,35 +50,6 @@ public class Process {
         return grayMatrix;
     }
 
-//    public static void toGrayscale(BufferedImage image) {
-//        try {
-//            BufferedImage grayImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-//            int width = image.getWidth();
-//            int height = image.getHeight();
-//            rbgMatrix = new int[width][height];
-//            WritableRaster raster = grayImage.getRaster();
-//            for (int x = 0; x < image.getWidth(); x++) {
-//                for (int y = 0; y < image.getHeight(); y++) {
-//                    int rgb = image.getRGB(x, y);
-//                    int r = (rgb >> 16) & 0xFF;
-//                    int g = (rgb >> 8) & 0xFF;
-//                    int b = rgb & 0xFF;
-//                    int gray = (int) (0.299 * r + 0.587 * g + 0.114 * b);
-//                    int grayRgb = (gray << 16) | (gray << 8) | gray;
-//                    rbgMatrix[x][y] = gray;
-//                    raster.setSample(x, y, 0, gray);
-//                    //grayImage.setRGB(x, y, grayRgb);
-//                }
-//            }
-//            File output = new File(grayPath);
-//            ImageIO.write(grayImage, "png", output);
-//
-//            System.out.println("Ảnh grayscale <" + grayPath + "> đã được lưu thành công!");
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     public static void toImage(int[][] matrix, String fileName) throws IOException {
         int width = matrix.length;
@@ -97,73 +67,6 @@ public class Process {
         File output = new File(outPath);
         ImageIO.write(image, "png", output);
         System.out.println("ảnh thành công!");
-    }
-
-    public static void createGrayCircle(int size){
-        try {
-
-            int[][] grayMatrix = new int[size][size];
-
-            // Tọa độ tâm và bán kính
-            int centerX = size / 2;
-            int centerY = size / 2;
-            double radius = size / 2;
-
-            // Xuất ma trận ra ảnh
-            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
-            WritableRaster raster = image.getRaster();
-            for (int y = 0; y < size; y++) {
-                for (int x = 0; x < size; x++) {
-                    double distance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2));
-                    int grayValue = (distance <= radius) ? (int) (255 - (distance *  255/ radius) ) : 0;
-                    raster.setSample(x, y, 0, grayValue); // Đặt giá trị grayscale trực tiếp
-                }
-            }
-
-            // Lưu ảnh ra file
-
-            File output = new File("circle_gray.png");
-            ImageIO.write(image, "png", output);
-
-            System.out.println("Đã tạo ma trận và lưu ảnh thành công!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeMatrixToFile(int[][] matrix,String filePath) {
-        try {
-            FileWriter writer = new FileWriter(filePath);
-            int width = matrix.length;
-            int height = matrix[0].length;
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-//                    writer.write(matrix[x][y] + "\t");
-                    writer.write(String.format("%-4d ", matrix[x][y]));
-                }
-                writer.write("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeMatrixToFile(double[][] matrix,String filePath) {
-        try {
-            FileWriter writer = new FileWriter(filePath);
-            int width = matrix.length;
-            int height = matrix[0].length;
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-//                    writer.write(matrix[x][y] + "\t");
-                    writer.write(String.format("%-4.2f ", matrix[x][y]));
-                }
-                writer.write("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
